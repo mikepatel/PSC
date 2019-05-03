74d86883a33de0188890ae903b23287878204731
Michael Patel <mikepatel@users.noreply.github.com>
2019-05-02 21:17:13 -0400

    Adding for initial Trump Net project

---
+++ trump_net.py
@@ -0,0 +1,23 @@
+""""
+John Jefferson III and Michael Patel
+September 2018 -
+Python 3.6.5
+TF
+
+Project Description:
+    - Text Generator modelling using RNNs
+
+Dataset: Trump tweets from https://www.kaggle.com/kingburrito666/better-donald-trump-tweets/version/2
+
+Notes:
+    - using tf.keras and eager execution
+    - character based model
+
+"""
+################################################################################
+# IMPORTs
+import tensorflow as tf
+
+
+################################################################################
+print(tf.__version__)
