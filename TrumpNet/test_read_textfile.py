8baad9bd19ccf2a36c732fcf1055aabd9d65feea
Michael Patel <mikepatel@users.noreply.github.com>
2019-05-02 20:36:57 -0400

    Delete test_read_textfile.py

--- TrumpNet/test_read_textfile.py
+++
@@ -1,8 +0,0 @@
-import os
-
-current_dir = os.getcwd()
-filename = "tweets.txt"
-
-file = os.path.join(current_dir, filename)
-text = [line.rstrip("\n") for line in open(file, encoding="utf8")]
-print(text[:10])
