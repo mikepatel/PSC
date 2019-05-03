5b17f4031763159bc0bfb10e9f8dd8fde013db87
Michael Patel <mikepatel@users.noreply.github.com>
2019-05-02 21:30:34 -0400

    Adding read csv

--- trump_net.py
+++ trump_net.py
@@ -18,6 +18,35 @@ Notes:
 # IMPORTs
 import tensorflow as tf
 
+import os
+import csv
+
 
 ################################################################################
-print(tf.__version__)
+# Data Preprocessing
+
+# build tweets list
+def build_tweets_list():
+    tweets_data_csv = os.path.join(os.getcwd(), "DonaldTrumpTweetsDataset_JustTweetText.csv")
+    tweets = []
+    with open(tweets_data_csv, newline="", encoding="utf8") as f:
+        csv_reader = csv.reader(f, delimiter=",")
+
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:  # column names
+                line_count += 1
+            else:
+                tweets.append(str(row))
+                line_count += 1
+
+    print(len(tweets))
+    return tweets
+
+
+################################################################################
+# Main
+if __name__ == "__main__":
+    print("\nTF version: {}".format(tf.__version__))
+    TWEETS = build_tweets_list()
