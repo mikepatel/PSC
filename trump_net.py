414025c8ac7ddd6ccbc4800b39254d4af0a73284
Michael Patel <mikepatel@users.noreply.github.com>
2019-05-05 19:18:45 -0400

    Adding class Dataset for tweets

--- trump_net.py
+++ trump_net.py
@@ -24,14 +24,24 @@ import re
 
 
 ################################################################################
-# Data Preprocessing
+tweets_data_csv = os.path.join(os.getcwd(), "DonaldTrumpTweetsDataset_JustTweetText.csv")
+filename = "DJT_tweets_noURLs.csv"
+tweets_data_no_url_csv = os.path.join(os.getcwd(), filename)
 
-# build tweets list
-def build_tweets_list():
-    tweets_data_csv = os.path.join(os.getcwd(), "DonaldTrumpTweetsDataset_JustTweetText.csv")
-    tweets = []
-    with open(tweets_data_csv, newline="", encoding="utf8") as f:
-        csv_reader = csv.reader(f, delimiter=",")
+
+################################################################################
+# Data Preprocessing
+class Dataset:
+    def __init__(self):
+        self.tweets = []
+        self.build_tweets_csv()
+        self.build_tweets_list()
+
+    # preprocesses tweets and writes output to csv
+    @staticmethod
+    def build_tweets_csv():
+        csv_reader = csv.reader(open(tweets_data_csv, mode="r", encoding="utf8"))
+        csv_writer = csv.writer(open(tweets_data_no_url_csv, mode="w", newline="", encoding="utf8"))
 
         line_count = 0
 
@@ -39,17 +49,30 @@ def build_tweets_list():
             if line_count == 0:  # column names
                 line_count += 1
             else:
-                tweet = str(row[0]).strip()
-                tweet = re.sub(
+                tweet = str(row[0]).rstrip()
+                tweet = str(re.sub(
                     "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
                     "",
                     tweet
-                )
-                tweets.append(tweet)  # first column in csv
+                )).rstrip()
+                csv_writer.writerow([tweet])
                 line_count += 1
 
-    print("Number of tweets in dataset: {}".format(len(tweets)))
-    return tweets
+    # build list of cleaned tweets
+    def build_tweets_list(self):
+        csv_reader = csv.reader(open(tweets_data_no_url_csv, mode="r", encoding="utf8"))
+
+        for row in csv_reader:
+            tweet = str(row[0]).rstrip()
+            self.tweets.append(tweet)
+
+    # get list of cleaned tweets
+    def get_tweets(self):
+        return self.tweets
+
+    # get number of cleaned tweets
+    def get_num_tweets(self):
+        return len(self.tweets)
 
 
 ################################################################################
@@ -60,6 +83,10 @@ def build_tweets_list():
 # Main
 if __name__ == "__main__":
     print("\nTF version: {}".format(tf.__version__))
-    TWEETS = build_tweets_list()
+
+    d = Dataset()
+    tweets = d.tweets
+    print(d.get_num_tweets())
+
 
 
