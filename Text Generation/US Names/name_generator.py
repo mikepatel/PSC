d196ded7e95fdeb6046325c17bc5ac2b858c0d24
Michael Patel <michael.patel94@gmail.com>
2019-06-19 18:14:51 -0400

    Adding data etl

--- Text Generation/US Names/name_generator.py
+++ Text Generation/US Names/name_generator.py
@@ -29,9 +29,18 @@ import tensorflow as tf
 
 ################################################################################
 # Model hyperparameters
-
+NUM_EPOCHS = 1
+BATCH_SIZE = 64
+MAX_SEQ_LENGTH = 20
+EMBEDDING_DIM = 256
+NUM_RNN_UNITS = 1024
+BUFFER_SIZE = 10000
+CHECKPOINT_PERIOD = NUM_EPOCHS  # how frequently to save checkpoints
 
 # Generation parameters
+START_STRING = "A"
+NUM_CHAR_GEN = 8  # number of generated characters
+TEMPERATURE = 0.8
 
 
 ################################################################################
@@ -91,4 +100,43 @@ if __name__ == "__main__":
 
     # convert list into one string (char list)
     names = "\n".join(names_list)
-    print(names)
+
+    # Tokenization: string => char tokens
+    unique_chars = sorted(set(names))
+    vocab_size = len(unique_chars)
+    print("Number of unique characters: {}".format(vocab_size))
+
+    # Vectorization: convert char to vectors of int
+    # create mapping from char to int
+    char2idx = {u: i for i, u in enumerate(unique_chars)}
+    # print("Char2idx mappings: {}".format(char2idx))
+
+    # create mapping from int to char
+    idx2char = {i: u for i, u in enumerate(unique_chars)}
+    # print("Idx2char mappings: {}".format(idx2char))
+
+    # create input and target sequences
+    input_seqs = []
+    target_seqs = []
+
+    # build list of sequences of indices
+    for i in range(0, len(names)-MAX_SEQ_LENGTH, MAX_SEQ_LENGTH):
+        # create batches of char
+        input_chars = names[i: i+MAX_SEQ_LENGTH]
+        target_chars = names[i+1: i+1+MAX_SEQ_LENGTH]
+
+        # convert each char in batch to int
+        input_seqs.append([char2idx[i] for i in input_chars])
+        target_seqs.append([char2idx[t] for t in target_chars])
+
+    # shape: (n, MAX_SEQ_LENGTH) where n is the number of index sequences
+    print("Shape of input sequences: {}".format(str(np.array(input_seqs).shape)))
+    print("Shape of target sequences: {}".format(str(np.array(target_seqs).shape)))
+
+    # use tf.data.Dataset to create batches and shuffle => TF model
+    # (features, labels) == (input_seqs, target_seqs)
+    sequences = tf.data.Dataset.from_tensor_slices((input_seqs, target_seqs))
+    sequences = sequences.shuffle(buffer_size=BUFFER_SIZE)
+    sequences = sequences.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+
+    print("Shape of batches: {}".format(sequences))
