a3c972a8658ed344b1b4079ed9c9d026a631573f
Michael Patel <michael.patel94@gmail.com>
2019-06-23 09:46:45 -0400

    400 epochs still too much

--- Text Generation/US Names/name_generator.py
+++ Text Generation/US Names/name_generator.py
@@ -29,7 +29,7 @@ import tensorflow as tf
 
 ################################################################################
 # Model hyperparameters
-NUM_EPOCHS = 550
+NUM_EPOCHS = 400
 BATCH_SIZE = 64
 MAX_SEQ_LENGTH = 10
 EMBEDDING_DIM = 512
