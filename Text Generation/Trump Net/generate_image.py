3777cdc9827e28e6d3498e67197732323f330f99
Michael Patel <michael.patel94@gmail.com>
2020-06-29 13:23:02 -0400

    Adding profile pic

--- Text Generation/Trump Net/generate_image.py
+++ Text Generation/Trump Net/generate_image.py
@@ -10,9 +10,57 @@ File description:
 """
 ################################################################################
 # Imports
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 ################################################################################
 # Main
 if __name__ == "__main__":
-    quit()
+    url = "https://www.tweetgen.com/create/tweet.html"
+    profile_pic_filepath = os.path.join(os.getcwd(), "trump_profile.jpg")
+
+    chromedriver_filepath = os.path.join(os.getcwd(), "chromedriver.exe")
+    #chrome_options = Options
+    driver = webdriver.Chrome(
+        chromedriver_filepath
+        #options=chrome_options
+    )
+
+    driver.get(url=url)
+
+    # Theme (light)
+
+    # Profile Picture
+    profile_pic_upload = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "pfpInput"))
+    )
+    profile_pic_upload.send_keys(profile_pic_filepath)
+
+    # Name
+
+    # Username
+
+    # Verified User
+
+    # Tweet Content
+
+    # Image (skip)
+
+    # Time
+
+    # Date
+
+    # Retweets
+
+    # Likes
+
+    # Client (skip)
+
+    # Generate Image and Download
+
+    #driver.close()
