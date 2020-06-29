1f33813c27534da528a3197d29562e8dd2a92bd6
Michael Patel <michael.patel94@gmail.com>
2020-06-29 13:04:18 -0400

    Adding new file to generate tweet image

---
+++ Text Generation/Trump Net/generate_image.py
@@ -0,0 +1,18 @@
+"""
+Michael Patel
+June 2020
+
+Project description:
+    Trump tweet generator
+
+File description:
+    Use Tweetgen.com to generate a tweet image
+"""
+################################################################################
+# Imports
+
+
+################################################################################
+# Main
+if __name__ == "__main__":
+    quit()
