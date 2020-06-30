5d59440964a258a1e6db1cc6b372e61c0213353c
Michael Patel <michael.patel94@gmail.com>
2020-06-30 00:57:05 -0400

    Successfully downloaded generated image

--- Text Generation/Trump Net/generate_image.py
+++ Text Generation/Trump Net/generate_image.py
@@ -11,6 +11,10 @@ File description:
 ################################################################################
 # Imports
 import os
+from datetime import datetime
+import urllib.request
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -43,25 +47,85 @@ if __name__ == "__main__":
     profile_pic_upload.send_keys(profile_pic_filepath)
 
     # Name
+    name = "Donald J. Trump"
+    name_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "nameInput"))
+    )
+    name_element.send_keys(name)
 
     # Username
+    username = "realDonaldTrump"
+    username_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "usernameInput"))
+    )
+    username_element.send_keys(username)
 
     # Verified User
+    verify_checkbox = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[1]/form/div[6]/div/label"))
+    )
+    verify_checkbox.click()
 
     # Tweet Content
+    tweet_content = "MAGA"
+    tweet_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "tweetTextInput"))
+    )
+    tweet_box.send_keys(tweet_content)
 
     # Image (skip)
 
     # Time
+    time = datetime.now().strftime("%H:%M")
+    time_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "time"))
+    )
+    time_element.send_keys(time)
 
     # Date
+    # Day
+    day = datetime.now().strftime("%d")
+    day_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "dayInput"))
+    )
+    day_element.send_keys(day)
+
+    # Month
+    month = datetime.now().strftime("%m")
+    month_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "monthInput"))
+    )
+    month_element.send_keys(month)
+
+    # Year
+    year = datetime.now().strftime("%Y")
+    year_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "yearInput"))
+    )
+    year_element.send_keys(year)
 
     # Retweets
+    randomize_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[1]/form/div[11]/div[2]/button"))
+    )
+    randomize_button.click()
 
     # Likes
 
     # Client (skip)
 
     # Generate Image and Download
+    generate_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="downloadButton"]'))
+    )
+    generate_button.click()
 
-    #driver.close()
+    # download generated image
+    sleep(1)
+    gen_image = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "imageOutput"))
+    )
+    src = gen_image.get_attribute("src")
+    urllib.request.urlretrieve(src, "generated.png")
+
+    driver.close()
